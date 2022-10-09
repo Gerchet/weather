d05a1b70352d404d73f55c3f506d159cacd587c0
Gerchet <konstantingerchet@gmail.com>
2022-10-09 19:42:29 +0300

    подготовил окружение

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <h2 className="title"><i classname="fa fa-cloud"></i>Weather App</h2>
+        </div>
+    </div>
+  );
+}
+
+export default App;
