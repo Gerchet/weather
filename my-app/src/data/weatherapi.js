48f2e20580678ccd867ed72a162f85392d67b55d
Gerchet <konstantingerchet@gmail.com>
2022-10-18 22:02:37 +0300

    changed temperature, canged city

--- my-app/src/data/weatherapi.js
+++ my-app/src/data/weatherapi.js
@@ -1,11 +1,10 @@
-import axios from 'axios'
-// записываем данные API
+import axios from 'axios';
+
 const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?'
 const apiKey = '38e7eb14b397d40b4a042564a33ffd72'
 
 export const getWeatherData = async (cityname) => {
   try {
-    // получаем данные API
     const {data} = await axios.get(baseUrl + `q=${cityname}&appid=${apiKey}`)
     return data
   } catch (error) {
